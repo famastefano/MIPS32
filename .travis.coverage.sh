e58267176ba45c5f234c33af89ddffca6aad7c42
Stefano Famà <famastefano@gmail.com>
2018-07-15 14:29:37 +0200

    - Testing fix for codecov

--- .travis.coverage.sh
+++
@@ -1,9 +0,0 @@
-if [ -z ${COVERAGE+x} ]; then
-# Creating report
-    cd ${TRAVIS_BUILD_DIR}
-    lcov --directory . --capture --output-file coverage.info coverage info
-    lcov --remove coverage.info '/usr/*' --output-file coverage.info 
-    lcov --list coverage.info
-# Uploading report to CodeCov
-    bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
-fi;
